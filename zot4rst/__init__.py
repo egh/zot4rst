0361000450c61163d4d803a26612cec44de98191
Erik Hetzner <egh@e6h.org>
2012-05-24 00:34:21 -0700

    zot4rst.ZoteroFootnoteSort -> xciterst.directives.FootnoteSortTransform

--- zot4rst/__init__.py
+++ zot4rst/__init__.py
@@ -151,87 +151,9 @@ class ZoteroSetupDirective(docutils.parsers.rst.Directive):
         if xciterst.citeproc.in_text_style:
             return []
         else:
-            pending = docutils.nodes.pending(ZoteroFootnoteSort)
+            pending = docutils.nodes.pending(xciterst.directives.FootnoteSortTransform)
             self.state_machine.document.note_pending(pending)
             return [pending]
-
-class ZoteroFootnoteSort(docutils.transforms.Transform):
-    default_priority = 641
-
-    def apply(self):
-        # Footnotes inserted via xcite are numbered before
-        # normal reST auto-numbered footnotes, so we renumber
-        # them as a single set, according to order of appearance
-        # of the refs in text, taking care to keep the
-        # ref and footnote numbering lined up.
-        footnotemap = {}
-        footnotes = self.document.autofootnotes
-        for i in range(0, len(self.document.autofootnotes), 1):
-            footnotemap[footnotes[i]['ids'][0]] = i
-        newlist = []
-        refs = self.document.autofootnote_refs
-        for i in range(0, len(refs), 1):
-            newlist.append(footnotes[footnotemap[refs[i]['refid']]])
-        self.document.autofootnotes = newlist
-
-        # The lists are now congruent and in document order, but the
-        # footnote numbers are screwed up, and the notes themselves
-        # may be in the wrong position.
-
-        # Reassign numbers to the footnotes
-        for i in range(0, len(self.document.autofootnotes), 1):
-            label = self.document.autofootnotes[i].children[0]
-            oldnum = label.children[0]
-            newnum = docutils.nodes.Text(str(i + 1))
-            label.replace(oldnum, newnum)
-
-        # Move the footnotes themselves to a more sensible location
-        # get the footnote label
-        for i in range(0, len(self.document.autofootnotes), 1):
-            footnote_node = self.document.autofootnotes[i]
-            ref_node = self.document.autofootnote_refs[i]
-
-            footnote_node.parent.remove(footnote_node)
-
-            footnotes_at_end = getattr(self.document.settings, 'footnotes_at_end', 0)
-
-            if footnotes_at_end:
-                self.document += footnote_node
-                self.document.setup_child(footnote_node)
-            else:
-                ref_parent = ref_node.parent
-                ref_and_note = docutils.nodes.generated()
-                ref_and_note += ref_node
-                ref_and_note.setup_child(ref_node)
-                ref_and_note += footnote_node
-                ref_and_note.setup_child(footnote_node)
-                ref_parent.replace(ref_node, ref_and_note)
-                ref_parent.setup_child(ref_and_note)
-
-        # Reassign numbers to the refs
-        # (we don't touch these until now because they may contain
-        # trojan footnotes)
-        for i in range(0, len(self.document.autofootnote_refs), 1):
-            ref = self.document.autofootnote_refs[i]
-            if len(ref.children) == 2:
-                ref.children.pop(0)
-            oldnum = ref.children[0]
-            newnum = docutils.nodes.Text(str(i + 1))
-            ref.replace(oldnum, newnum)
-
-        # Reset note numbers
-        xciterst.citeproc.citations = None
-        xciterst.cluster_tracker.reset()
-        for i in range(0, len(self.document.autofootnotes), 1):
-            footnote = self.document.autofootnotes[i]
-            content = footnote.children[1].children[0]
-            if isinstance(footnote.children[1].children[0], docutils.nodes.pending):
-            	cluster = content.details['cite_cluster']
-		xciterst.cluster_tracker.track(cluster)
-                cluster.note_index = i
-
-        empty = docutils.nodes.generated()
-        self.startnode.replace_self(empty)
 
 class ZoteroCitationTransform(docutils.transforms.Transform):
     #
