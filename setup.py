e01b3ac1313e058c45760467c17510e6d7f03151
Erik Hetzner <egh@e6h.org>
2013-06-09 12:55:14 -0700

    new version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name             = 'zot4rst',
-      version          = '0.2',
+      version          = '0.3pre',
       description      = 'Zotero for reStructuredText',
       author           = 'Erik Hetzner',
       author_email     = 'egh@e6h.org',
