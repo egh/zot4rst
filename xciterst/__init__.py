a85f80297a07835896cd057503436a9829ef0ac0
Erik Hetzner <egh@e6h.org>
2013-07-05 18:17:31 -0700

    remove unused reset method

--- xciterst/__init__.py
+++ xciterst/__init__.py
@@ -30,9 +30,6 @@ class ClusterTracker(object):
         index = len(self.clusters) - 1
         cluster.index = index
 
-    def reset(self):
-        self.clusters = []
-
 # tracker for clusters
 cluster_tracker = ClusterTracker()
 
