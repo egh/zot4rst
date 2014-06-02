32fdfc93041fce0e31ad59bab4e434730491a1b1
Dan MacKinlay <myopenid@email.possumpalace.org>
2014-06-02 21:23:02 +0200

    reflect current pelical signal names

--- zot4rst/pelican_plugin.py
+++ zot4rst/pelican_plugin.py
@@ -4,7 +4,7 @@ import xciterst
 
 def register():
     signals.article_generator_init.connect(setup_zotero)
-    signals.article_generate_preread.connect(article_setup_zotero)
+    signals.article_generator_preread.connect(article_setup_zotero)
 
 def setup_zotero(generator):
     zot4rst.init(generator.settings.get('CITATION_STYLE', zot4rst.DEFAULT_CITATION_STYLE))
