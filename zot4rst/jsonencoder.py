e9046eba2ac7947b9fbda2b5ae9184fa86417433
Erik Hetzner <egh@e6h.org>
2015-08-03 19:38:38 -0700

    Quiet pylint

--- zot4rst/jsonencoder.py
+++ zot4rst/jsonencoder.py
@@ -7,20 +7,26 @@ class ZoteroJSONEncoder(json.JSONEncoder):
         if isinstance(obj, CitationInfo):
             retval = {}
             # need to make a decision here at some point
-            if obj.citekey: 
+            if obj.citekey:
                 retval['easyKey'] = obj.citekey
             elif obj.id:
                 retval['id'] = obj.id
-            if obj.prefix: retval['prefix'] = "%s "%(obj.prefix) # ensure spaces in prefix, suffix
-            if obj.suffix: retval['suffix'] = " %s"%(obj.suffix)
-            if obj.label: retval['label'] = obj.label
-            if obj.locator: retval['locator'] = obj.locator
-            if obj.suppress_author: retval['suppress-author'] = obj.suppress_author
-            if obj.author_only: retval['author-only'] = obj.author_only
+            if obj.prefix:
+                retval['prefix'] = "%s "%(obj.prefix) # ensure spaces in prefix, suffix
+            if obj.suffix:
+                retval['suffix'] = " %s"%(obj.suffix)
+            if obj.label:
+                retval['label'] = obj.label
+            if obj.locator:
+                retval['locator'] = obj.locator
+            if obj.suppress_author:
+                retval['suppress-author'] = obj.suppress_author
+            if obj.author_only:
+                retval['author-only'] = obj.author_only
             return retval
         elif isinstance(obj, CitationCluster):
-            return { 'citationItems': obj.citations,
-                     'properties'   : { 'index'     : obj.index,
-                                        'noteIndex' : obj.note_index } }
+            return {'citationItems': obj.citations,
+                    'properties'   : {'index'     : obj.index,
+                                      'noteIndex' : obj.note_index}}
         else: return super(ZoteroJSONEncoder, self).default(self, obj)
 
